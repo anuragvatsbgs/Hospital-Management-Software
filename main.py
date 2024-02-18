f14f6ebb3cec7901d31292c3f1cf49d71e268560
ANURAG KUMAR <anuragvatsbgs@gmail.com>
2024-02-18 17:39:01 +0530

    admin

--- main.py
+++ main.py
@@ -3,8 +3,10 @@ from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager
 from kivy.core.window import Window
 import mysql.connector
-
+from kivymd.uix.snackbar import Snackbar
+from kivy.metrics import dp
 from kivy.utils import platform
+from kivymd.uix.menu import MDDropdownMenu
 class hospitalmanagementsystem(MDApp):
     global screen_manager
     screen_manager = ScreenManager()
@@ -17,6 +19,48 @@ class hospitalmanagementsystem(MDApp):
         screen_manager.add_widget(main)                         # Add Main screen with screenmanager
         self.theme_cls.theme_style_switch_animation = True      # animation=true
         self.theme_cls.primary_palette ="BlueGray"              # Theme color
+        menu_items = [
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Password",
+                "font_style" : 'Body1',
+                "height": dp(45),
+                "on_release": lambda x="Settings": self.reset_pasd(),
+            },
+            
+            {
+                "viewclass": "OneLineListItem",
+                "text":"FAQ",
+                "font_style" : 'Body1',
+                "height": dp(45),
+                "on_release": lambda x="Settings": self.faq(),
+            },
+            {
+                "viewclass": "OneLineListItem",
+                "text": "About Us",
+                "font_style" : 'Body1',
+                "height": dp(45),
+                "on_release": lambda x="Settings": self.about_us(),
+            },
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Help & Support",
+                "font_style" : 'Body1',
+                "height": dp(45),
+                "on_release": lambda x="Settings": self.help_support(),
+            },
+            {
+                "viewclass": "OneLineListItem",
+                "text": "Log Out",
+                "font_style" : 'Body1',
+                "height": dp(45),
+                "on_release": lambda x="Log Out": self.log_out(),
+            }]
+        self.menu = MDDropdownMenu(
+            items=menu_items,
+            width_mult=3,
+        )
+
         return screen_manager                                   # Return screen manager
     def on_start(self):
         screen_manager.current="LOGIN"
@@ -28,6 +72,10 @@ class hospitalmanagementsystem(MDApp):
         screen_manager.current="LOGIN"
     def log_out(self):
         screen_manager.current="LOGIN"
+        self.menu.dismiss()
+    def dot(self, button):
+        self.menu.caller = button
+        self.menu.open()
 if __name__ == "__main__":
     if platform == "win":
         hospitalmanagementsystem().run()
